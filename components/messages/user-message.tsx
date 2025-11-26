5aafc2f5bb2b97212025fcf17741cc7a59ef07bc
sidhantbishnoi12 <sidhantbishnoi@gmail.com>
2025-11-26 13:42:49 +0530

    Update user-message.tsx

--- components/messages/user-message.tsx
+++ components/messages/user-message.tsx
@@ -1,19 +1,28 @@
+// components/messages/user-message.tsx
 import { UIMessage } from "ai";
 import { Response } from "@/components/ai-elements/response";
 
 export function UserMessage({ message }: { message: UIMessage }) {
-    return (
-        <div className="whitespace-pre-wrap w-full flex justify-end">
-            <div className="max-w-lg w-fit px-4 py-3 rounded-[20px] bg-neutral-100">
-                <div className="text-sm">
-                    {message.parts.map((part, i) => {
-                        switch (part.type) {
-                            case "text":
-                                return <Response key={`${message.id}-${i}`}>{part.text}</Response>;
-                        }
-                    })}
-                </div>
-            </div>
+  return (
+    <div className="w-full flex">
+      <div className="ml-auto max-w-[70%]">
+        <div className="bubble-user">
+          <div className="text-sm">
+            {message.parts.map((part, i) => {
+              switch (part.type) {
+                case "text":
+                  return (
+                    <Response key={`${message.id}-${i}`}>
+                      {part.text}
+                    </Response>
+                  );
+                default:
+                  return null;
+              }
+            })}
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
